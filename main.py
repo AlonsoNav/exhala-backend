0970040970ef2949f3a6abb5e5c833eae1aaca3f
Alonso <106555422+AlonsoNav@users.noreply.github.com>
2025-02-19 00:04:00 -0600

    Fix: need render deploy id

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.user import user_router as user
 from routes.psychologist import psychologist_router as psychologist
-
+import requests
 app = FastAPI()
 
 app.add_middleware(
@@ -16,3 +16,8 @@ app.add_middleware(
 # Routes
 app.include_router(user)
 app.include_router(psychologist)
+
+@app.get("/my-ip")
+def get_ip():
+    ip = requests.get("https://api64.ipify.org?format=json").json()["ip"]
+    return {"server_ip": ip}
